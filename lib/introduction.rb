9ba705417799b109eb87a5be94e058097a571012
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-06-04 03:14:35 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,7 +5,7 @@ end
 introduction
 
 def introduction_2(name_2="Dan", language="Ember.js")
-  puts "Hi, my name is #{name}, and I am learning to program in #{language}."
+  puts "Hi, my name is #{name_2}, and I am learning to program in #{language}."
 end
 
 introduction_2
