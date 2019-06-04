7037caab0a86bf5a0a2e921d721527b9f49f194f
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-06-04 03:16:34 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,11 @@
+def introduction(name="Josh")
+  puts "Hi, my name is #{name}."
+end
+
+introduction
+
+def introduction_with_language(name_2="Dan", language="Ember.js")
+  puts "Hi, my name is #{name_2} and I am learning to program in #{language}."
+end
+
+introduction_with_language
